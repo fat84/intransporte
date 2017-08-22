e3a7912aa98d3d695e2d93624555f92b60e511a7
Fabio E. Moreno <fabiomoreno@outlook.com>
2017-08-21 20:10:19 -0500

    Terminado reportes actuales, solo falta el de cartera

--- app/DespachoDetalle.php
+++ app/DespachoDetalle.php
@@ -10,6 +10,6 @@ class DespachoDetalle extends Model
     protected $table = "despacho_detalle";
     protected $fillable = ['despacho_id','producto_id','cantidad','unidad','valor_unidad','porc_descuento','descuento','porc_impuesto','impuesto',''];
     public function Despacho(){
-        return $this->belongsTo('intransporte\Despacho');
+        return $this->belongsTo('intransporte\Despacho', 'despacho_id', 'id');
     }
 }
